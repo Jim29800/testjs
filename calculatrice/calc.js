c9770c87dc5af0d9a65afbd37f378b22c841d6fc
Jim29800 <jbouvier1173@gmail.com>
2017-07-04 15:13:32 +0200

    simplification function

--- calculatrice/calc.js
+++ calculatrice/calc.js
@@ -1,16 +1,16 @@
-// partie 1
-var nombre = 0;
-var addition = 0;
-var multiplication = 0;
+
+var nombre = Number(prompt("Merci de saisir votre nombre de départ"));
+var addition = Number(prompt("quel est le nombre que vous voulez lui additionner ?"));
+var multiplication = Number(prompt("Par quel nombre voulez-vous multiplier ?"));
 var resultAddi = 0;
 var resultMulti = 0;
 
+
+/* partie 1
 nombre = Number(prompt("Merci de saisir votre nombre de départ"));
 addition = Number(prompt("quel est le nombre que vous voulez lui additionner ?"));
-multiplication= Number(prompt("Par quel nombre voulez-vous multiplier ?"));
+multiplication = Number(prompt("Par quel nombre voulez-vous multiplier ?"));
 
-/*
-Suite de la partie 1
 resultAddi= (nombre) + (addition);
 resultMulti= (nombre) * (multiplication);
 alert("le resultat de votre addition : " + nombre +"+"+ addition + "=" + resultAddi + "\nle resultat de votre multiplication : " + nombre +"x"+ multiplication + "=" + resultMulti );
@@ -20,13 +20,13 @@ Partie 2 avec les fonction :*/
 
 function addi(n1, n2){
 	var result;
-	result = (n1 + n2)
-	return result
+	result = n1 + n2;
+	return result;
 };
 function multi(n1,n2){
 	var result;
-	result = (n1 * n2)
-	return result
+	result = n1 * n2;
+	return result;
 };
 
 resultAddi= addi(nombre, addition) ;
