bfe96eddc3ad2478c75cff95a610f4e6774eca57
Jim29800 <jbouvier1173@gmail.com>
2017-07-05 17:01:45 +0200

    exercice 4

--- GoToTheDom/GototheDOM/js/app.js
+++ GoToTheDom/GototheDOM/js/app.js
@@ -8,3 +8,20 @@ el.appendChild(el_content);
 
 var remove = document.getElementById('remove');
 remove.parentNode.removeChild(remove);
+
+var couleur = document.getElementById('changeColor');
+couleur.style.backgroundColor = "Red"
+
+var container = document.getElementById('addChildren');
+var creat = document.createElement("p");
+var ajoutP = container.insertBefore(creat, container.addChildren);
+
+
+function para (a,b){
+	for (var i = 0; i < b ; i++) {
+		return a;
+	};
+
+};
+
+para(ajoutP, 4)
